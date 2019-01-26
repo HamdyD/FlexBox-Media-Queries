6d42601d81c176adfa474fc5a0e6d0708f1f7bd2
HamdyD <hamdy.dakkak@gmail.com>
2019-01-26 12:16:13 +0100

    fixing the spacing between items

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,14 +4,14 @@ import "./Grid.css";
 export default class MyGrid extends React.Component {
   render() {
     return (
-      <div className="flex-container">
-        <div className="flex-left">Hi there</div>
+      <div className="main-container">
+        <div className="container-left">Hi there</div>
 
         <div className="container-right">
-          <div className="item-1">1</div>
-          <div className="item-2">2</div>
-          <div className="item-3">3</div>
-          <div className="item-4">4</div>
+          <div className="item item-1">1</div>
+          <div className="item item-2">2</div>
+          <div className="item item-3">3</div>
+          <div className="item item-4">4</div>
         </div>
       </div>
     );
